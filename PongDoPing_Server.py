0c1bc0fdca12e9d7e808d921cb60a8742fb39760
root <root@ServidorEastUS.servidoreastus.b6.internal.cloudapp.net>
2019-05-31 01:32:35 +0000

    Adaptando IPs

--- PongDoPing_Server.py
+++ PongDoPing_Server.py
@@ -1,6 +1,7 @@
 import socket
-HOST_IP = "127.0.0.1"     # Endereco IP do Servidor
-PORT = 5000         # Porta que o Servidor está
+
+HOST_IP = "10.0.0.5"
+PORT = 5000
 
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -8,6 +9,7 @@ orig = (HOST_IP, PORT)
 udp.bind(orig)
 
 while True:
+	print("Esperando...")
 	data, cliente = udp.recvfrom(1024)
 	print("Servidor recebeu de", str(cliente),":",data.decode())
 	udp.sendto(b"Pong", cliente)
